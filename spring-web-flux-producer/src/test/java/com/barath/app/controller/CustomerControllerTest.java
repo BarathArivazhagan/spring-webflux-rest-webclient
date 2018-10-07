00573b8e7a3a034536fb246ee3de1dbcdc757846
BarathArivazhagan <barathmacdec91@gmail.com>
2018-10-07 14:08:38 +0530

    enhanced controller tests

--- spring-web-flux-producer/src/test/java/com/barath/app/controller/CustomerControllerTest.java
+++ spring-web-flux-producer/src/test/java/com/barath/app/controller/CustomerControllerTest.java
@@ -1,6 +1,7 @@
 package com.barath.app.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.lang.invoke.MethodHandles;
@@ -14,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +35,10 @@ public class CustomerControllerTest extends AbstractSpringWebMVCRunner {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	private static final String CUSTOMER_BASE_ENDPOINT = "/customers";
+	private static final String CUSTOMER_ID="customerId";
+	private static final String CUSTOMER_NAME="customerName";
+	private static final String CUSTOMER_GENDER="customerGender";
+	private static final String JSON_PATH_LENGTH_EXPRESSION = "$.length()";
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -53,11 +59,16 @@ public class CustomerControllerTest extends AbstractSpringWebMVCRunner {
 	public void  testSaveCustomer() throws Exception {
 		
 		Customer customer = this.customerData().stream().findFirst().get();
-		this.mockMvc
+		MvcResult mvcResult = this.mockMvc
 			.perform( post(CUSTOMER_BASE_ENDPOINT.concat("/new"))
 					.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-					.content(JacksonUtils.toJson(customer)))
-			.andExpect(status().isOk());
+					.content(JacksonUtils.toJson(customer))).andReturn();
+
+		this.mockMvc.perform( asyncDispatch(mvcResult))
+				.andExpect(status().isOk())
+				.andExpect( jsonPath(CUSTOMER_ID).value(1L))
+				.andExpect( jsonPath(CUSTOMER_NAME).value("BARATH"))
+				.andExpect( jsonPath(CUSTOMER_GENDER).value("MALE"));
 		
 			
 			
@@ -67,10 +78,18 @@ public class CustomerControllerTest extends AbstractSpringWebMVCRunner {
 	public void testSaveCustomers() throws Exception {
 
 		List<Customer> customers = this.customerData();
-		this.mockMvc.perform( post(CUSTOMER_BASE_ENDPOINT)
+		MvcResult mvcResult =this.mockMvc.perform( post(CUSTOMER_BASE_ENDPOINT)
 						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
 							.content(JacksonUtils.toJson(customers)))
-				.andExpect(status().isOk()).andReturn();
+				.andReturn();
+		this.mockMvc.perform(asyncDispatch(mvcResult))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath(JSON_PATH_LENGTH_EXPRESSION).value(5))
+				.andExpect(jsonPath( "$[0].customerName").value("BARATH"))
+				.andExpect(jsonPath( "$[1].customerName").value("BARRY"))
+				.andExpect(jsonPath( "$[2].customerName").value("MALA"))
+				.andExpect(jsonPath( "$[3].customerName").value("MALAR"))
+				.andExpect(jsonPath( "$[4].customerName").value("VIRAT"));
 	}
 
 	@Test
@@ -78,20 +97,34 @@ public class CustomerControllerTest extends AbstractSpringWebMVCRunner {
 		Customer customer = this.customerData().stream().findFirst().get();
 		this.customerRepository.save(customer);
 		customer.setCustomerName("TEST");
-		this.mockMvc
+	     MvcResult mvcResult = this.mockMvc
 				.perform( put(CUSTOMER_BASE_ENDPOINT.concat("/update"))
 						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
 						.content(JacksonUtils.toJson(customer)))
-				.andExpect(status().isOk());
-
+				.andReturn();
+	     this.mockMvc.perform(asyncDispatch(mvcResult))
+					.andExpect(status().isOk())
+					.andExpect( jsonPath(CUSTOMER_ID).value(1L))
+					.andExpect( jsonPath(CUSTOMER_NAME).value("TEST"))
+					.andExpect( jsonPath(CUSTOMER_GENDER).value("MALE"));
 	}
 
 	@Test
 	public void testFindAllCustomers() throws Exception  {
 
 		this.customerRepository.saveAll(this.customerData());
-		this.mockMvc.perform(get(CUSTOMER_BASE_ENDPOINT))
-				.andExpect(status().isOk());
+		MvcResult mvcResult = this.mockMvc.perform(get(CUSTOMER_BASE_ENDPOINT))
+				.andReturn();
+
+		this.mockMvc.perform(asyncDispatch(mvcResult))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath(JSON_PATH_LENGTH_EXPRESSION).value(5))
+				.andExpect(jsonPath( "$[0].customerName").value("BARATH"))
+				.andExpect(jsonPath( "$[1].customerName").value("BARRY"))
+				.andExpect(jsonPath( "$[2].customerName").value("MALA"))
+				.andExpect(jsonPath( "$[3].customerName").value("MALAR"))
+				.andExpect(jsonPath( "$[4].customerName").value("VIRAT"));
+
 	}
 
 	@Test
@@ -100,8 +133,12 @@ public class CustomerControllerTest extends AbstractSpringWebMVCRunner {
 		Customer customer = this.customerData().stream().findFirst().get();
 		this.customerRepository.save(customer);
 		Long customerId = 1L;
-		this.mockMvc.perform(get(CUSTOMER_BASE_ENDPOINT.concat("/"+customerId)))
-				.andExpect(status().isOk());
+		MvcResult mvcResult = this.mockMvc.perform(get(CUSTOMER_BASE_ENDPOINT.concat("/"+customerId)))
+				.andReturn();
+		this.mockMvc.perform( asyncDispatch(mvcResult))
+					.andExpect(status().isOk())
+					.andExpect( jsonPath(CUSTOMER_ID).value(1L));
+
 	}
 
 
