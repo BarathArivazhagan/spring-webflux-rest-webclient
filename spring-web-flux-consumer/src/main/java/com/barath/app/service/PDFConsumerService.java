1b6e790ad19423509bf258aaf8d8c9736a8cb28f
Ingo Kegel <ingo.kegel@ej-technologies.com>
2021-12-09 12:08:53 +0100

    Spring Boot 2.6.1

--- spring-web-flux-consumer/src/main/java/com/barath/app/service/PDFConsumerService.java
+++ spring-web-flux-consumer/src/main/java/com/barath/app/service/PDFConsumerService.java
@@ -1,20 +1,13 @@
 package com.barath.app.service;
 
-import java.lang.invoke.MethodHandles;
-
+import com.barath.app.configuration.RestEndpoints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyExtractors;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import com.barath.app.configuration.RestEndpoints;
-import com.barath.app.model.Customer;
-import com.barath.app.util.JacksonUtils;
-
 import reactor.core.publisher.Mono;
+
+import java.lang.invoke.MethodHandles;
 
 @Service
 public class PDFConsumerService {
@@ -33,11 +26,7 @@ public class PDFConsumerService {
 	        }
          	return  this.webClient.post()
 	                    .uri(RestEndpoints.PDF_RETRIEVE_ENDPOINT)
-	                    .body(BodyInserters.fromObject(null))
-	                    .exchange()
-	                    .flatMap( response -> {
-	                    	return response.bodyToMono(String.class);
-						});
+	                    .exchangeToMono(response -> response.bodyToMono(String.class));
 
 	    }
 
